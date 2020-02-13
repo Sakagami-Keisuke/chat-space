4e5d5fa2120a1c5b7f2589b07812dae384e9d01f
坂上計介 <iwayasunset@gmail.com>
2020-02-13 17:27:56 +0900

    add_message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+  # before_action :set_group, only: :update
 
   def index
   end
@@ -19,21 +20,31 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+    
   end
 
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group) , notice: 'グループを更新しました'
     else
       render :edit
     end
   end
-
+# redairect_toのパスばPrefix名+このコントローラで作成するインスタンス変数@group(idにあるカラムデータを格納したもの)
 
   private
   def group_params
     params.require(:group).permit(:name, user_ids: [])
   end
+
+  # def set_group
+  #   @group = Group.find(params[:group_id])
+  # end
+
+# リファクタリングできること
+# いくつも同じ@group定義があるので
+# set_groupメソッドをbefore_action onlyで機能させれば、
+# 各アクションの@group = Group.find(params[:id])を消せる
 
 end
